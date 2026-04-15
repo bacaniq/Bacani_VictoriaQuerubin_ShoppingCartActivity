d8d9fc69dc19ceb389205f11ce73969770aa8b2d
bacaniq <bacaniq216@gmail.com>
2026-04-16 00:01:21 +0800

    Added cart management, stock deduction, and duplicate item checking

--- Bacani_shoppingcart/Program.cs
+++ Bacani_shoppingcart/Program.cs
@@ -1,6 +1,5 @@
 using System;
 
-
 class Program
 {
     static void Main(string[] args)
@@ -19,7 +18,9 @@ class Program
         products[6] = new Product { ID = 7, Name = "Teddy Bear", Price = 900, RemainingStock = 15 };
 
         bool running = true;
-
+        int cartCount = 0;
+        Product[] cart = new Product[5];
+        int[] quantities = new int[5];
 
         //Start ng Loop
         while (running)
@@ -43,7 +44,8 @@ class Program
             // EXIT
             if (inputid == 0)
             {
-                break;
+                Console.WriteLine("Exiting...");
+                return;
             }
 
             // FIND PRODUCT
@@ -64,7 +66,68 @@ class Program
                 Console.WriteLine("Invalid product!");
                 continue;
             }
-            
+
+            if (selectedProduct.RemainingStock == 0)
+            {
+                Console.WriteLine("Out of stock!");
+                continue;
+            }
+
+            //ASKING FOR QUANTITY
+            Console.Write("How many?: ");
+            int inputqty;
+
+            //Input validation
+            if (!int.TryParse(Console.ReadLine(), out inputqty) || inputqty <= 0)
+            {
+                Console.WriteLine("Invalid quantity!");
+                continue;
+            }
+
+            //checks if mas mataas ang quantity sa stock available
+            if (inputqty > selectedProduct.RemainingStock)
+            {
+                Console.WriteLine("Not enough stock available.");
+                continue;
+            }
+
+            // ADD TO CART LOGIC
+            int existing = -1;
+            for (int i = 0; i < cartCount; i++)
+            {
+                if (cart[i].ID == selectedProduct.ID)
+                {
+                    existing = i;
+                    break;
+                }
+            }
+
+            //checks if item already exists in the cart
+            if (existing != -1)
+            {
+                quantities[existing] += inputqty;
+                selectedProduct.DeductStock(inputqty); // Uses method in Product class
+                Console.WriteLine("Updated existing item in cart");
+            }
+            else
+            {
+                if (cartCount >= 5) //Checks if the items in the cart reaches the limit of 5
+                {
+                    Console.WriteLine("Cart is full.");
+                }
+                else
+                {
+                    // Store the selected product object into the cart array
+                    cart[cartCount] = selectedProduct;
+
+                    // Store the quantity entered by the user into the quantities array
+                    quantities[cartCount] = inputqty;
+                    cartCount++;
+
+                    selectedProduct.DeductStock(inputqty);
+                    Console.WriteLine("Added to cart!");
+                }
+            }
         } // end of loop
     }
 }
