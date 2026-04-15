7e0a4de3fd8addd836bbf0fdccc17910b2b42587
bacaniq <bacaniq216@gmail.com>
2026-04-15 21:35:42 +0800

    Create Product class and inventory methods

---
+++ Bacani_shoppingcart/Product.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Product
+{
+    public int ID;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{ID}. {Name} - ${Price} - (Stock: {RemainingStock})");
+
+    }
+    public double GetItemTotal(int quantity)
+    {
+        return Price * quantity;
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        return RemainingStock >= quantity;
+    }
+
+    public void DeductStock(int quantity)
+    {
+        RemainingStock -= quantity;
+    }
+}
